d75f14f92931c86c700adec007b1cf3e3c0a6f7c
szkutek <aga.szkutek@gmail.com>
2017-03-15 01:13:29 +0100

    PO lista 3

--- sem2/po/csharp-projects/lista3/zad3/TimeNTon.cs
+++ sem2/po/csharp-projects/lista3/zad3/TimeNTon.cs
@@ -1,9 +1,9 @@
 namespace TimeNTonLib{
 
-	sealed class TimeNTon{
+	sealed public class TimeNTon{
 
 		static int pos = 0;
-		static int N = 2;
+		static int N = 3;
 		static TimeNTon[] instance;
 		static TimeNTon instance2;
 
@@ -11,22 +11,24 @@ namespace TimeNTonLib{
 		TimeNTon(){ }
 		
 		public static TimeNTon Instance(double t){
-			if (instance == null)
-				instance = new TimeNTon[N];
-
-			if (t<10.0 && t>12.0) {
-				if (instance2 == null) instance2 = new TimeNTon();
+		
+			if (t<10.0 || t>12.0) {
+				if (instance2 == null) 
+					instance2 = new TimeNTon();
 				System.Console.WriteLine("pojedyncza instacja\n");
 				return instance2;
 			}
 			else {
+				if (instance == null)
+					instance = new TimeNTon[N];
+
 				if (instance[pos] == null){
 					instance[pos] = new TimeNTon();
 					System.Console.WriteLine("tworze obiekt nr {0}\n",pos);
 				}
 				TimeNTon tmp = instance[pos];
-				pos = (pos+1)%(N-1);
 				System.Console.WriteLine("zwracam obiekt nr {0}\n",pos);
+				pos = (pos+1)%N;
 				return tmp;
 			}
 
