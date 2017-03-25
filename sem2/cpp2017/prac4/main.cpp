071eeeae19c5ae193f18055bca60af453179f641
szkutek <aga.szkutek@gmail.com>
2017-03-25 15:54:59 +0100

    cpp prac4 plus wykłady

---
+++ sem2/cpp2017/prac4/main.cpp
@@ -0,0 +1,58 @@
+
+#include "string.h"
+#include <iostream>
+
+#include <stdexcept>
+#include <vector>
+
+// From the lecture. Not needed for the task:
+
+void fail_often() {
+    srand(time(NULL));
+    if (rand() & 1)
+        throw std::runtime_error("i failed");
+}
+
+void f() {
+    string s = "this is a string";
+
+    std::vector<string> vect = {"these", "are", "also", "string"};
+    string more[] = {"these", "are", "even", "more", "string"};
+
+    // fail_often( );
+}
+
+int main(int argc, char *argv[]) {
+    // Add more tests by yourself. Untested code = unwritten code.
+
+    string s;
+    string s2 = "hello";
+    std::cout << "s2 = " << s2 << "\n";
+
+    s = s2;  // Assignment, not initialization.
+    s = s;
+
+    std::cout << "s[2] = " << s[2] << "\n";
+
+    s += '2';
+    std::cout << "s = " << s << "\n";
+
+    s += s;
+    std::cout << "s = " << s << "\n";
+
+    string s3 = s2 + s2;
+    std::cout << "s3 = " << s3 << "\n\n";
+
+    s = "this is a string";
+    std::cout << s << "\n";
+    for (char &c : s)
+        c = toupper(c);
+    std::cout << "s = " << s << "\n";
+
+    std::cout << "s > s2 " << (s > s2) << "\n";
+    std::cout << "s2 == \"hello\" " << (s2 == "hello") << "\n";
+    std::cout << "s2 <= \"hello\" " << (s2 <= "hello") << "\n";
+
+}
+
+
---
+++ sem2/cpp2017/prac4/string.cpp
@@ -0,0 +1,68 @@
+
+#include "string.h"
+
+std::ostream &operator<<(std::ostream &out, const string &s) {
+    for (char c : s)
+        out << c;
+    return out;
+}
+
+void string::operator+=(char c) { // Append character at the end.
+    char *tmp = p;
+    p = new char[len + 1];
+    strncpy(p, tmp, len);
+    p[len] = c;
+    len++;
+    delete[] tmp;
+}
+
+void string::operator+=(const string &s) {
+    char *tmp = p;
+    p = new char[len + s.get_len()];
+    strncpy(p, tmp, len);
+    strncpy(p + len, s.p, s.get_len());
+    len += s.get_len();
+    delete[] tmp;
+}
+
+string operator+(string s1, const string &s2) {
+    s1 += s2;
+    return s1;
+}
+
+bool operator==(const string &s1, const string &s2) {
+    if (s1.get_len() != s2.get_len()) return false;
+
+    for (size_t i = 0; i < s1.get_len(); i++)
+        if (s1[i] != s2[i]) return false;
+
+    return true;
+}
+
+bool operator!=(const string &s1, const string &s2) {
+    return !(s1 == s2);
+}
+
+bool operator<(const string &s1, const string &s2) {
+    if (s1.get_len() < s2.get_len()) return true;
+
+    for (size_t i = 0; i < s1.get_len(); i++)
+        if (s1[i] >= s2[i]) return false;
+
+    return true;
+}
+
+bool operator>(const string &s1, const string &s2) {
+    return !(s1 < s2) && s1 != s2;
+}
+
+bool operator<=(const string &s1, const string &s2) {
+    return !(s1 > s2);
+}
+
+bool operator>=(const string &s1, const string &s2) {
+    return !(s1 < s2);
+}
+
+
+
