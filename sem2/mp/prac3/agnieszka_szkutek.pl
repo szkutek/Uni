abcbcd186fa604f52f1f2585f13c62ce39d543f0
szkutek <aga.szkutek@gmail.com>
2017-05-01 01:40:57 +0200

    parser v2

--- sem2/mp/prac3/agnieszka_szkutek.pl
+++ sem2/mp/prac3/agnieszka_szkutek.pl
@@ -1,10 +1,17 @@
 % W programie korzystam z fragmentów kodu autorstwa pana Tomasza Wierzbickego (while_parser.pl)
-:- module(agnieszka_szkutek, [parse/3]).
-
+% :- module(agnieszka_szkutek, [parse/3]).
+% [tokDef,tokVar(main),tokLBracket,tokVar('A'),tokRBracket,tokEq,tokVar('B')]
 
 parse(_, Codes, Program) :-
   phrase(lexer(TokList), Codes),
   phrase(program(Program), TokList).
+
+parse2(String, Program) :-
+  string_to_list(String, Codes),
+  phrase(lexer(TokList), Codes),
+  phrase(program(Program), TokList).
+
+
 
 
 lexer(Tokens) -->
@@ -99,69 +106,77 @@ definicja(Def) --> [tokDef],!, identyfikator(Id),
     [tokLBracket], wzorzec(Wz), [tokRBracket], [tokEq], wyrazenie(Wyr),
     {Def =.. [def, Id, Wz, Wyr]}.
 
-wzorzec(Wz) --> wzorzec(W1), [tokComma],!, wzorzec(W2),
-    Wz =.. [pair, no, W1, W2].
+wzorzec(wildcard(no)) --> [tokWildcard],!.
+wzorzec(W) --> zmienna(W),!.
 wzorzec(Wz) --> [tokLBracket],!, wzorzec(Wz),
     [tokRBracket].
-wzorzec(W) --> zmienna(W),!.
-wzorzec(wildcard(no)) --> [tokWildcard].
+wzorzec(Wz) --> wzorzec(W1), [tokComma],!, wzorzec(W2),
+    {Wz =.. [pair, no, W1, W2]}.
 
 
 wyrazenie(Wyr) --> [tokIf],!, wyrazenie(E1), [tokThen],
     wyrazenie(E2), tokElse, wyrazenie(E3),
-    Wyr =.. [if, no, E1, E2, E3].
+    {Wyr =.. [if, no, E1, E2, E3]}.
 wyrazenie(Wyr) --> [tokLet],!, wzorzec(P), '=',
     wyrazenie(E1), tokIn, wyrazenie(E2),
-    Wyr =.. [let, no, P, E1, E2].
+    {Wyr =.. [let, no, P, E1, E2]}.
 wyrazenie(W) --> wyrazenie_op(W).
 
-wyrazenie_op(Wyr) --> wyrazenie_op(W1),
-    op_binarny(TokOp, Op),!, wyrazenie_op(W2),
-    Wyr =.. [op, no, Op, W1, W2].
-wyrazenie_op(Wyr) --> op_unarny(Op),!, wyrazenie_op(W),
-    Wyr =.. [op, no, Op, W].
-wyrazenie_op(Wyr) --> wyrazenie_proste(Wyr).
+
+% wyrazenie_op(Wyr) --> wyrazenie_proste(Wyr).
+% wyrazenie_op(Wyr) --> op_unarny(Op),!, wyrazenie_op(W),
+%     Wyr =.. [op, no, Op, W].
+% wyrazenie_op(Wyr) --> wyrazenie_op(W1),
+%     op_binarny(Op),!, wyrazenie_op(W2),
+%     Wyr =.. [op, no, Op, W1, W2].
+
+wyrazenie_op(Wyr) -->
+    w1(W1), op_binarny_2(Op),!, w1(W2),
+    {Wyr =.. [op, no, Op, W1, W2]}
+    ; w1(Wyr).
+w1(Wyr) -->
+    w2(W1), op_binarny_3(Op),!, w1(W2),
+    {Wyr =.. [op, no, Op, W1, W2]}
+    ; w2(Wyr).
+w2(Wyr) --> w2(W1), op_binarny_4(Op),!, w3(W2),
+    {Wyr =.. [op, no, Op, W1, W2]}
+    ; w3(Wyr).
+w3(Wyr) -->
+    w3(W1), op_binarny_5(Op),!, w_unarne(W2),
+    {Wyr =.. [op, no, Op, W1, W2]}.
+w_unarne(Wyr) -->
+    op_unarny(Op),!, wyrazenie_proste(W1),
+    {Wyr =.. [op, no, Op, W1]}
+    ; wyrazenie_proste(Wyr).
 
 
-
-op_unarny(TokOp, Op) -->
-    member( (TokOp, Op), [ (tokMinus, '-'),
+op_unarny(Op) -->
+    [TokOp],
+    {member( (TokOp, Op), [ (tokMinus, '-'),
                            (tokHash, '#'),
-                           (tokTilde, '~')] ).
+                           (tokTilde, '~')] )}.
 
-op_binarny_2(TokOp, Op) -->
-    member( (TokOp, Op), [ (tokEq, '='),
+op_binarny_2(Op) -->
+    [TokOp],
+    {member( (TokOp, Op), [ (tokEq, '='),
                            (tokNeq, '<>'),
                            (tokLt, '<'),
                            (tokLeq, '<='),
                            (tokGt, '>'),
-                           (tokGeq, '>=')] ).
-op_binarny_3(tokAt, '@').
-op_binarny_4(TokOp, Op) -->
-    member( (TokOp, Op), [ (tokVLine, '|'),
+                           (tokGeq, '>=')] )}.
+op_binarny_3('@') --> [tokAt].
+op_binarny_4(Op) -->
+    [TokOp],
+    {member( (TokOp, Op), [ (tokVLine, '|'),
                            (tokCaret, '^'),
                            (tokPlus, '+'),
-                           (tokMinus, '-')] ).
-op_binarny_5(TokOp, Op) -->
-    member( (TokOp, Op), [ (tokAnd, '&'),
+                           (tokMinus, '-')] )}.
+op_binarny_5(Op) -->
+    [TokOp],
+    {member( (TokOp, Op), [ (tokAnd, '&'),
                            (tokTimes, '*'),
                            (tokDiv, '/'),
-                           (tokPercent, '%')] ).
-
-% op_binarny_4(op(Pos,N)) -->
-% [(Pos,Tok)],
-% { member(
-% (Tok,N),
-% [ (tokVBar, '|'),
-% (tokCaret, '^'),
-% (tokPlus, '+'),
-% (tokMinus, '-')
-% ] ) }.
-
-
-
-
-
+                           (tokPercent, '%')] )}.
 
 
 
@@ -173,12 +188,12 @@ wyrazenie_proste(W) --> wyrazenie_atomowe(W),!.
 
 wybor_bitu(W) --> wyrazenie_proste(E1), [tokLSqBracket],
     wyrazenie(E2), [tokRSqBracket],
-    W =.. [bitsel, no, E1, E2].
+    {W =.. [bitsel, no, E1, E2]}.
 wybor_bitow(W) --> wyrazenie_proste(E1),
     [tokLSqBracket],
     wyrazenie(E2), [tokDots], wyrazenie(E3),
     [tokRSqBracket],
-    W =.. [bitsel, no, E1, E2, E3].
+    {W =.. [bitsel, no, E1, E2, E3]}.
 
 wyrazenie_atomowe(W) --> zmienna(W),!.
 wyrazenie_atomowe(W) --> wywolanie_funkcji(W),!.
@@ -190,12 +205,12 @@ zmienna(W) --> identyfikator(W).
 
 wywolanie_funkcji(W) --> identyfikator(Name),
     [tokLBracket], wyrazenie(E), [tokRBracket],
-    W =.. [call, no, Name, E].
+    {W =.. [call, no, Name, E]}.
 
 pusty_wektor(empty(no)) --> [tokLBracket], [tokRBracket].
 
 pojedynczy_bit(bit(no, E)) --> [tokLBracket], wyrazenie(E), [tokRBracket].
 
-identyfikator(Id) --> tokVar(Id).
+identyfikator(var(no,Id)) --> [tokVar(Id)].
 
-literal_calkowitoliczbowy(N) --> tokNumber(N).
+literal_calkowitoliczbowy(N) --> [tokNumber(N)].
