3083d4e30feddb76c35e0fe6c1fd4e3c027c18d5
szkutek <aga.szkutek@gmail.com>
2017-05-20 15:11:40 +0200

    lista 9 pliki od prowadzacego

---
+++ sem2/cpp2017/prac9/powerproduct.cpp
@@ -0,0 +1,54 @@
+
+#include "powerproduct.h"
+#include <algorithm>
+
+
+std::ostream& operator << ( std::ostream& out, const powvar & p )
+{
+   if( p.n == 0 )
+   {
+      out << "1"; // Should not happen, but we still have to print something.
+      return out;
+   }
+
+   out << p.v;
+   if( p.n == 1 )
+      return out;
+
+   if( p.n > 0 )
+      out << "^" << p.n;
+   else
+      out << "^{" << p.n << "}";
+   return out;
+}
+
+
+std::ostream& operator << ( std::ostream& out, const powerproduct& c )
+{
+   if( c. isunit( ))
+   {
+      out << "1";
+      return out;
+   }
+
+   for( auto p = c. repr. begin( ); p != c. repr. end( ); ++ p )
+   {
+      if( p != c. repr. begin( ))
+         out << ".";
+      out << *p;
+   }
+
+   return out;
+}
+
+
+int powerproduct::power( ) const 
+{
+   int p = 0;
+   for( auto pv : repr )
+      p += pv. n;
+   return p;
+}
+
+
+
